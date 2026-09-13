187bd1f95d65c2f02a48c456ac9c2f9ef3c8e55b
Scott Riley <hugs@scott.is>
2026-09-13 02:34:24 +0100

    better font weights for mono

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -51,6 +51,11 @@
 	/>
 	<link rel="stylesheet" href="https://use.hugeicons.com/font/icons.css" />
 	<link rel="icon" href={favicon} />
+	<link rel="preconnect" href="https://fonts.bunny.net" />
+	<link
+		href="https://fonts.bunny.net/css?family=lilex:200,200i,300,300i,400,400i,500i,600,600i,700i"
+		rel="stylesheet"
+	/>
 	<title>Lorist; the writing app that gets out of your way</title>
 	<meta property="og:image" content="https://lorist.app/og.png" />
 	<meta property="og:title" content="Lorist; the writing app that gets out of your way" />
